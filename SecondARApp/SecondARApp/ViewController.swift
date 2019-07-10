b5f6504c5f534900d6f9c6fc45cd13c01301d7b4
Anjali Reddy <anjred@amazon.com>
2019-07-10 15:32:57 -0700

    Initial upload

---
+++ SecondARApp/SecondARApp/ViewController.swift
@@ -0,0 +1,157 @@
+//
+//  ViewController.swift
+//  SecondARApp
+//
+//  Created by Reddy, Anjali on 6/21/19.
+//  Copyright © 2019 Reddy, Anjali. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        sceneView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    func tapped(_ gesture: UITapGestureRecognizer) {
+        guard let currentFrame = self.sceneView.session.currentFrame else {
+            return
+        }
+        
+        // create a web view
+        let webView = UIWebView(frame: CGRect(x: 0, y: 0, width: 640, height: 480))
+        let request = URLRequest(url: URL(string: "http://www.youtube.com")!)
+        
+        webView.loadRequest(request)
+        let tvPlane = SCNPlane(width: 1.0, height: 0.75)
+        tvPlane.firstMaterial?.diffuse.contents = webView
+        tvPlane.firstMaterial?.isDoubleSided = true
+        
+        let tvPlaneNode = SCNNode(geometry: tvPlane)
+        
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1.0
+        
+        tvPlaneNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        tvPlaneNode.eulerAngles = SCNVector3(0,0,0)
+        
+        self.sceneView.scene.rootNode.addChildNode(tvPlaneNode)
+    }
+    
+    func addItemToPosition(_ position: SCNVector3) {
+        let scene = SCNScene(named: "art.scnassets/ship.scn")
+        
+        DispatchQueue.main.async {
+            if let node = scene?.rootNode.childNode(withName: "ship", recursively: false) {
+                node.position = position
+                self.sceneView.scene.rootNode.addChildNode(node)
+            }
+        }
+    }
+    
+    @objc
+    func didTap(_ gesture: UITapGestureRecognizer) {
+        let sceneViewTappedOn = gesture.view as! ARSCNView
+        let touchCoordinates = gesture.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates, types: .existingPlaneUsingExtent)
+        
+        guard !hitTest.isEmpty, let hitTestResult = hitTest.first else {
+            return
+        }
+        
+        let position = SCNVector3(hitTestResult.worldTransform.columns.3.x,
+                                  hitTestResult.worldTransform.columns.3.y,
+                                  hitTestResult.worldTransform.columns.3.z)
+        
+        addItemToPosition(position)
+    }
+    
+   
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Enable horizontal plane detection
+        configuration.planeDetection = .horizontal
+        
+        // show Feature Points
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/**
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if let planeAnchor = anchor as? ARPlaneAnchor {
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.75)
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.position = SCNVector3Make(planeAnchor.center.x, planeAnchor.center.x, planeAnchor.center.z)
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let planeAnchor = anchor as? ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane {
+            plane.width = CGFloat(planeAnchor.extent.x)
+            plane.height = CGFloat(planeAnchor.extent.z)
+            planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
+        }
+    }
+**/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+        
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
